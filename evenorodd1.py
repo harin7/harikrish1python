f292b6e025a3f4dc596dec7b688a9cbdd301df5f
harin7 <41772252+harin7@users.noreply.github.com>
2018-07-27 14:42:27 +0530

    Rename evenorodd.py to evenorodd1.py

---
+++ evenorodd1.py
@@ -0,0 +1,8 @@
+#this program used to find whether the given number is even or odd#
+n=int(raw_input())
+if(n%2==0):
+     print("even")
+ elif(n%2==1):
+     print("odd")
+ else:
+     print("invalid")
