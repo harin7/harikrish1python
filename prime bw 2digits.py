ae6afba60ab34058d14ae87664d9f5d9636bf805
harin7 <41772252+harin7@users.noreply.github.com>
2018-08-08 14:52:57 +0530

    Update prime bw 2digits.py

--- prime bw 2digits.py
+++ prime bw 2digits.py
@@ -4,7 +4,7 @@ x,y=int(x),int(y)
 for j in range(x,y):
     count=0
     for i in range(1,j+1):
-        if(j%i)==0:
+        if (j%i)==0:
             count+=1
     if(count==2):
         print(j,end=" ")
