dd96be1c1c58ff3d880c9df42ea3cc7df2a344df
harin7 <41772252+harin7@users.noreply.github.com>
2018-08-08 14:47:24 +0530

    Create prime bw 2digits.py

---
+++ prime bw 2digits.py
@@ -0,0 +1,10 @@
+x,y=input().split()
+x,y=int(x),int(y)
+
+for j in range(x,y):
+    count=0
+    for i in range(1,j+1):
+        if(j%i)==0:
+            count+=1
+    if(count==2):
+        print(j,end=" ")
