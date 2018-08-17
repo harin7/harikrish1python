c40dbca1d5e063075106d1bb04e6349e233a1844
harin7 <41772252+harin7@users.noreply.github.com>
2018-08-17 21:23:20 +0530

    Update pyr21.py

--- pyr21.py
+++ pyr21.py
@@ -1,7 +1,7 @@
-x1,y1=map(int,input().split())
-x2,y2=map(int,input().split())
-x3,y3=map(int,input().split())
-if((x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2))==0):
+h1,i1=map(int,input().split())
+h2,i2=map(int,input().split())
+h3,i3=map(int,input().split())
+if((h1*(i2-i3)+h2*(i3-i1)+h3*(i1-i2))==0):
     print ("yes")
 else:
     print ("no")
